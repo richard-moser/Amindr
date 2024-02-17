39a6b3131fd4cd42e0184b2c5174d50d1778a68b
Richard Moser <richardmoser@Macbook.local>
2024-02-17 09:19:13 +0100

    -

--- labels.py
+++ labels.py
@@ -34,27 +34,32 @@ Dont't wrap ```json ``` around!
 
 
 A_subcategory_descriptions = """
-(A1) Genetics 
-This topic includes new variants, genetic and transcriptomics insights, APOE, and familial AD mutations.
-(A2) Autophagy/Proteostasis
-This topic includes endosomal/lysosomal dysfunction, protein uptake/trafficking, mitophagy, general protein aggregation, post-translational modifications, and proteomics. It does not include tau and amyloid-beta, as it belongs to a different topic.
-(A3) Metabolism and Mitochondrial Dysfunction/Oxidative Stress
-This topic is about metabolism and mitochondrial dysfunction/oxidative stress
-(A4) Neural Excitability, Synapses, and Brain Connectivity
-This topic includes synaptic transmission and neurotransmitters, and brain connectivity (functional) and structural integrity.
-(A5) Amyloid Pathology
-This topic includes APP processing/secretases, amyloid-beta aggregation, and amyloid-beta function and amyloid-mediated toxicity/pathology.
-(A6) Tau Pathology
+Genetics (A1a)
+This topic includes new variants, genetic and transcriptomics insights such as genome-wide association studies.
+APOE (A1b) 
+This topic is about apolipoprotein E (APOE). 
+Familial AD mutations (A1c) 
+This topic is about familial AD mutations, such as APP, presenilin 1/2, BACE
+Autophagy/Proteostasis (A2) 
+This topic includes endosomal/lysosomal dysfunction, protein uptake/trafficking, general protein aggregation, post-translational modifications, and proteomics. It does not include tau and amyloid-beta, as it belongs to a different topic.
+Metabolism and Mitochondrial Dysfunction/Oxidative Stress (A3) 
+This topic includes metabolic changes, nutrition/diet, stress, mitochondria, oxidative stress, changes in glucose/insulin. 
+Neural Excitability, Synapses, and Brain Connectivity (A4) 
+This topic is on synaptic transmission and neurotransmitters, including cholinergic and dopaminergic systems. It also includes brain connectivity and structural integrity.
+Amyloid Pathology (A5) 
+This topic includes APP processing/secretases, amyloid-beta aggregation, and amyloid-beta function and amyloid-mediated toxicity/pathology. Check if another subcategory fits before labelling with this one.
+Tau Pathology (A6) 
 This topic includes tau post-translational modifications/aggregations, and tau seeding and pathology.
-(A7) The Immune System and Glial Cells
-This topic includes the gut/brain axis, peripheral and neuroinflammation, TREM2, astrocyte/microglia focused papers, and oligodendrocyte papers.
-(A8) Cognitive and Behavioural Changes
-This topic focuses on cognitive and behavioral changes. It includes depression, sleep, smell, and auditory/visual processing. 
-(A9) Vascular Contributions
+The Immune System and Glial Cells (A7) 
+This topic includes the gut/brain axis, inflammation, TREM2. Also, it includes studies that focus on astrocytes, microglia, or oligodendrocytes.
+Cognitive and Behavioural Changes (A8) 
+This topic focuses on cognitive and behavioral changes. It includes sensory processing, depression, sleep, smell, auditory, visual functioning. 
+Vascular Contributions (A9) 
 This topic includes arterial stiffness, atherosclerosis, cerebral amyloid angiopathy, microbleeds, white matter hyperintensities, blood-brain barrier, endothelial cells, lipoproteins interacting with vessels, angiogenesis, and platelet aggregation.
-(A10) Epidemiological Studies 
+Epidemiological Studies (A10) 
 This topic includes non-genetic factors or comorbidities that put people at risk of developing AD or decrease the risk. It includes incidence/prevalence studies in specific populations, genetic predisposition to a risk factor, comorbidities such as cardiovascular disease/diabetes/depression, nutrition/vitamin deficiencies, socioeconomic background, air pollution, etc. Note: Mechanistic studies relating to HOW these non-genetic factors/comorbidities put people at risk belong to the other categories depending on their fit.
-
+Miscallaneous (AX)
+Comprehensive umbrella for all publications that do not fit clearly to any of the other categories.
 """
 
 def get_prompt_A (title, body, categories_explained=A_subcategory_descriptions) :
@@ -64,15 +69,37 @@ TITLE: {title}.
 ABSTRACT: {body}.
 ABSTRACT END
 You can ONLY choose from the following curated categories:
-Categoriy shortcuts: [A1, A2, A3, A4, A5, A6, A7, A8, A9, A10]
+Categoriy shortcuts: [A1a, A1b, A1c, A2, A3, A4, A5, A6, A7, A8, A9, A10, AX]
 Explanations: {categories_explained}
-Use the following JSON template to answer
-Replace y with the most suitable category shortcut and z with a reasoning for your choice. 
+Use the following JSON template to answer.
+Add all suitable categories to the categories list, starting with the most suitable one.
+For each category, replace y with the category shortcut and z with a reasoning for your choice. 
 ALWAYS ONLY respond with a valid json for python with the following structure:
 {{
-"most_relevant_category": y, 
-"clear_reasoning": z
+categories: [
+{{"category": y, "clear_reasoning": z}},
+]
 }}
 Dont't wrap ```json ``` around!
 """.format(title=title, body=body, categories_explained=categories_explained)
     return prompt
+
+
+
+
+C_subcategory_descriptions = """
+(C1) title
+descpription 
+
+
+
+"""
+
+
+
+D_subcategory_descriptions = """
+(D1)
+
+
+
+"""
